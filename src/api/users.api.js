10eeb972b9446681dd55e01841490d7be360fc86
ShaNaimShourov <shanaim2k15@gmail.com>
2023-03-18 16:26:37 +0600

    chore : 🎇db populate

--- src/api/users.api.js
+++ src/api/users.api.js
@@ -2,7 +2,11 @@ import fetchData from "./fetch.api";
 
 export async function getAllUsers() {
 	try {
-		const result = await fetchData("https://jsonplaceholder.typicode.com/posts?_page=2&_limit=20");
+		const local =
+			"https://my-json-server.typicode.com/ShaNaim/blog_frontend_jukto_interview_2023/posts/1";
+		const serv = "https://jsonplaceholder.typicode.com/posts?_page=2&_limit=20";
+
+		const result = await fetchData(local);
 		console.log({ result });
 	} catch (error) {
 		console.error({ error });
